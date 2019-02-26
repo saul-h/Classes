536445ead2391f43a4972d37120733dd07e3dbeb
Abel Acosta <abelacosta1596@gmail.com>
2019-02-25 16:26:13 -0800

    Created abstract class.

--- src/Coin.java
+++ src/Coin.java
@@ -1,4 +1,14 @@
 
-public class Coin {
-
+public abstract class Coin {
+	
+	protected int value; // in cents
+	
+	public void setValue(int v) {
+		this.value = v;
+	}
+	
+	public int getValue() {
+		return this.value;
+	}
+	
 }
