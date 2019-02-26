076d0df9c38292061bf4723ef4276e7b95477c36
Abel Acosta <abelacosta1596@gmail.com>
2019-02-25 18:22:28 -0800

    Added toString method to display vending machine contents.

--- src/VendingMachine.java
+++ src/VendingMachine.java
@@ -12,4 +12,16 @@ public class VendingMachine {
 		inventory.add(new Inventory(product, quantity));
 	}
 	
+	public String toString() {
+		String header = String.format("%-10s%8s%10s%n", "Name", "Quantity", "Price");
+		
+		String body = "";
+		for(int i = 0; i < inventory.size(); i++) {
+			Inventory item = inventory.get(i);
+			body += String.format("%-10s%8d%10.2f%n", item.getProduct().getName(), item.getQuantity(), (double)item.getProductCost() / 100f);
+		}
+		
+		return header + body;
+	}
+	
 }
