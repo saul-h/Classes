9bc6574cee96ed5c907dd761379342aa3cf5e7d5
Abel Acosta <abelacosta1596@gmail.com>
2019-02-25 16:27:07 -0800

    VendingMachine holds an ArrayList of Inventory.

--- src/VendingMachine.java
+++ src/VendingMachine.java
@@ -1,4 +1,15 @@
+import java.util.ArrayList;
 
 public class VendingMachine {
-
+	
+	private ArrayList<Inventory> inventory;
+	
+	public VendingMachine() {
+		inventory = new ArrayList<Inventory>();
+	}
+	
+	public void addItem(Product product, int quantity) {
+		inventory.add(new Inventory(product, quantity));
+	}
+	
 }
