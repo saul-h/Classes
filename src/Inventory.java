8b78604f36501cbc6a4bc6333a476315cfc1e33b
Abel Acosta <abelacosta1596@gmail.com>
2019-02-25 16:29:05 -0800

    Inventory class holds Product and quantity information.

---
+++ src/Inventory.java
@@ -0,0 +1,24 @@
+
+public class Inventory {
+	
+	private Product product;
+	private int quantity;
+	
+	public Inventory(Product p, int q) {
+		this.product = p;
+		this.quantity = q;
+	}
+	
+	public Product getProduct() {
+		return product;
+	}
+	
+	public int getQuantity() {
+		return quantity;
+	}
+	
+	public int getProductCost() {
+		return product.cost;
+	}
+	
+}
