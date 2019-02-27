76e91619a047c8f872e1617bb2f6518460e9fac7
Abel Acosta <abelacosta1596@gmail.com>
2019-02-26 16:05:46 -0800

    Removed use of User object since it no longer exists.

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,6 @@
 public class Main {
 
 	public static void main(String[] args) {
-		
-		User user = new User(16, 5, 20, 5);
-		
 		// Create Vending Machine object
 		VendingMachine vendingMachine = new VendingMachine();
 	}
