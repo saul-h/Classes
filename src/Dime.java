a61c0ee6356231627dd95a853160fa0c8c0f7caa
saul-h <saulhernandez217@gmail.com>
2019-03-05 10:51:07 -0800

    Added documentation for Coins classes

--- src/Dime.java
+++ src/Dime.java
@@ -1,4 +1,11 @@
+/**
+ * Extends Coin class. This is a coin of type Dime with value 10 cents.
+ */
 public class Dime extends Coin {
+	/**
+	 * Default constructor for the Dime class. Value attribute (inherited from the Coin class) is set equal to
+	 * 10
+	 */
 	public Dime() {
 		this.value = 10;
 	}
--- src/Nickel.java
+++ src/Nickel.java
@@ -1,4 +1,11 @@
+/**
+ * Extends Coin class. This is a coin of type Nickel with value 5 cents.
+ */
 public class Nickel extends Coin {
+	/**
+	 * Default constructor for the Nickel class. Value attribute (inherited from the Coin class) is set equal to
+	 * 5 cents
+	 */
 	public Nickel() {
 		this.value = 5;
 	}
