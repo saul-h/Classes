4b40ec0c4ffeb991e04522e98fad107a822378f8
Abel Acosta <abelacosta1596@gmail.com>
2019-03-05 09:31:53 -0800

    Added more to tester class.

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,46 @@
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
+		
+		Scanner input = new Scanner(System.in);
+		
 		// Create Vending Machine object
 		VendingMachine vendingMachine = new VendingMachine();
 		
 		Product p1 = new Product("Cheetos", 100);
+		Product p2 = new Product("Snickers", 120);
+		Product p3 = new Product("Peanuts", 125);
+		
 		vendingMachine.addItem(p1, 10);
-		vendingMachine.addItem(p1, 10);
-		System.out.println(vendingMachine.toString());
+		vendingMachine.addItem(p2, 2);
+		vendingMachine.addItem(p3, 4);
+		
+		System.out.println("S)how products  I)nsert coin B)uy  \nA)dd product  R)emove coins  Q)uit");
+		
+		String resp = input.nextLine();
+		
+		switch(resp){
+		case "s":
+			System.out.println(vendingMachine);
+			break;
+		case "i":
+			//TODO: Insert coin to vending machine
+			break;
+		case "b":
+			//TODO: Remove product from vending machine and remove inserted coins
+			break;
+		case "a":
+			addProduct();
+			break;
+		case "r":
+			//TODO: Remove inserted coins in vending machine
+			break;
+		}
+	}
+	
+	static void addProduct() {
+		
 	}
 }
